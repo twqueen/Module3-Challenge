343c045a3fe88ebfb36f630677fc65b412af4ee6
twqueen <tiffanyytsai@gmail.com>
2023-01-02 20:09:27 -0800

    added some comments for anyone who wants to study my thought process when developing the codes

--- Develop/script.js
+++ Develop/script.js
@@ -9,16 +9,19 @@ var numbersOnly = "0123456789"
 function generatePassword() {
   var newPassword = ""
   var Userwants = ""
+  //establish desired password length
   var pwLength = window.prompt("Please enter your desired password length (between 8 to 128 characters)");
   if (!pwLength) {
-    return;
+    return; //if user chose to cancel
   }
   if ((pwLength >= 8) && (pwLength <= 128)) {
-    var ConfirmSpecial = window.confirm("Do you want to include Special Characters? i.e !, @, #, $");
+    //prompt the user for desired characters in their password
+    var ConfirmSpecial = window.confirm("Do you want to include Special Characters? \ni.e !, @, #, $");
     var ConfirmLower = window.confirm("Do you want to include Lowercase letters?");
     var ConfirmUpper = window.confirm("Do you want to include Uppercase letters?");
     var ConfirmNumber = window.confirm("Do you want to include numbers?");
 
+    //add var strings into one var for computer to choose from
     if (ConfirmSpecial) {
       Userwants += specialcharactersOnly;
     }
@@ -36,6 +39,8 @@ function generatePassword() {
       window.alert("Please select at least one character type.");
     }
 
+    //establish a situation where at least one of each confirmed characters will be in the password.
+    //random password will be generated in this step
     for (var i = 0; i < pwLength; i++) {
       if (i == 0 && ConfirmSpecial) {
         var randomNumber = Math.floor(Math.random() * specialcharactersOnly.length);
@@ -63,8 +68,11 @@ function generatePassword() {
     window.alert("Please enter a value between 8 and 128");
   }
 
+  //newly generated password will be displayed
   return newPassword
+  
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
